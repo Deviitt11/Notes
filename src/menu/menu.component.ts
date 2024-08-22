36eb8c967379998eb86aac3beef7f3a836ac977c
Deviitt11 <david.collados11@gmail.com>
2024-08-22 17:26:29 +0200

    Frontend - 22ago

---
+++ src/menu/menu.component.ts
@@ -0,0 +1,12 @@
+/*import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    template: '<App-menu />',
+    imports :[MenuComponent]
+})
+export class MenuComponent {
+    
+}
+*/
